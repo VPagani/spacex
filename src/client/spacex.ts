291371597e8425692f5e178a6a5feeaa50e0e527
VPagani <xvpagani@gmail.com>
2023-02-09 15:59:05 -0300

    Add basic index page to display API data

---
+++ src/client/spacex.ts
@@ -0,0 +1,14 @@
+import type { Result } from "@/types/result";
+import type { Launch, Launches } from "@/services/spacex";
+
+export { Launch, Launches };
+
+function request<T>(url: string): Promise<Result<T>> {
+	return fetch(url, { next: { revalidate: 20 } }).then((res) => res.json());
+}
+
+export const getLaunch = (id: string) => request<Launch>(`/api/launches/${id}`);
+export const getLatestLaunch = () => request<Launch>("/api/launches/latest");
+export const getNextLaunch = () => request<Launch>("/api/launches/next");
+export const getPastLaunches = () => request<Launches>("/api/launches/past");
+export const getUpcomingLaunches = () => request<Launches>("/api/launches/upcoming");
